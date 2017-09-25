ae1859be379988af0d2293cc8cc5ccad30c2ddfc
Brian Maher <brian.maher@kcl.ac.uk>
2017-09-25 10:36:35 +0000

    Fix for population

--- mmc/wagtail_hooks.py
+++ mmc/wagtail_hooks.py
@@ -5,40 +5,44 @@ import re
 def populate_entity_relationships(request, page):
     # I know this shouldn't go here, but it's a bit of
     # trickery
-    from cms.models import (Entity, ObjectPage) # noqa
+    try:
+        from cms.models import (Entity, ObjectPage) # noqa
 
-    if isinstance(page, ObjectPage):
+        if isinstance(page, ObjectPage):
 
-        # Clear existing relations
-        page.entities.clear()
+            # Clear existing relations
+            page.entities.clear()
 
-        # Our list of new potential entities:
-        page_list = []
+            # Our list of new potential entities:
+            page_list = []
 
-        # Regex to match:
-        re1 = '.*?'
-        re2 = '(\\d+)'
+            # Regex to match:
+            re1 = '.*?'
+            re2 = '(\\d+)'
 
-        rg = re.compile(re1 + re2, re.IGNORECASE | re.DOTALL)
+            rg = re.compile(re1 + re2, re.IGNORECASE | re.DOTALL)
 
-        # Check for new entities
-        for block in page.body.stream_data:
-            q = None
-            if type(block) is tuple:
-                q = block[1].source
-            else:
-                if block['type'] == 'paragraph':
-                    q = block['value']
-            if q:
-                search = rg.search(q)
-                if search:
-                    page_id = int(search.group(1))
-                    if page_id not in page_list:
-                        page_list.append(page_id)
-        # We have our list:
-        entities = Entity.objects.filter(id__in=page_list)
-        for entity in entities.all():
-            page.entities.add(entity)
+            # Check for new entities
+            for block in page.body.stream_data:
+                q = None
+                if type(block) is tuple:
+                    q = block[1].source
+                else:
+                    if block['type'] == 'paragraph':
+                        q = block['value']
+                if q:
+                    search = rg.search(q)
+                    if search:
+                        page_id = int(search.group(1))
+                        if page_id not in page_list:
+                            page_list.append(page_id)
+            # We have our list:
+            entities = Entity.objects.filter(id__in=page_list)
+            for entity in entities.all():
+                page.entities.add(entity)
+
+    except:
+        pass
 
 
 hooks.register('after_create_page', populate_entity_relationships)
