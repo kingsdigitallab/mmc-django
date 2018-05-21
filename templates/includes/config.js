cb28070913c5841919f6897dfabb0de45fc0a3b1
Tiffany Ong <tiffany.ong@kcl.ac.uk>
2018-05-21 16:21:41 +0000

    navigation checked, mobile unchecked

--- templates/includes/config.js
+++ templates/includes/config.js
@@ -2,3 +2,12 @@ window.GOOGLE_ANALYTICS = {
     'ga_id': '{{ GA_ID }}',
     'domain': '{{ request.META.HTTP_HOST }}',
 };
+
+
+// BM - Please check 
+window.addEventListener('resize', function() {
+    if (window.matchMedia('(min-width: 400px)').matches) {
+        document.getElementById('menu-switch').checked = false;
+    }
+}, true);
+
