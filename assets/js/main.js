3e1b4be95977fcae7a42a851d166952a4fd1979f
Brian Maher <brian.maher@kcl.ac.uk>
2018-06-05 08:53:59 +0000

    cookie policy alert

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,48 @@
-// Main
-require([
-    'requirejs',
-    'jquery',
-    'fn',
-    'ga'
-], function(r, $) {
-    'use strict';
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+function eraseCookie(name) {   
+    document.cookie = name+'=; Max-Age=-99999999;';  
+}
 
-    $(document).ready(function() {
-    });
 
+$(document).ready(function()
+{
+	window.addEventListener('resize', function() {
+	    if (window.innerWidth < 768) {
+	        document.getElementById('menu-switch').checked = false;
+	    } else
+	    {
+	    	document.getElementById('menu-switch').checked = true;
+	    }
+	}, true);
+
+	$('body').on('click', '#privacy-alert-agree', function(event)
+	{
+		event.preventDefault();
+		setCookie('privacy-agree', 'set');
+		$('#privacy-alert').slideUp();
+	});
+
+	if(getCookie('privacy-agree') !== "set")
+	{
+		$('#privacy-alert').slideDown();
+	}
 });
+
