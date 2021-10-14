13bf9064993e9261124e355530cd08005a3af843
Miguel Vieira <jmvieira@pm.me>
2021-10-14 14:37:52 +0100

    👌 Improve: Remove unused components; remove requirejs, use just compressor.

--- mmc/settings/base.py
+++ mmc/settings/base.py
@@ -7,17 +7,17 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 For production settings see
 https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 """
-from ddhldap.settings import * # noqa
-
-import os
-import logging
 import getpass
+import logging
+import os
+
+from ddhldap.settings import *  # noqa
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
-PROJECT_NAME = 'mmc'
-PROJECT_TITLE = 'Mozart and Material Culture'
+PROJECT_NAME = "mmc"
+PROJECT_TITLE = "Mozart and Material Culture"
 
 # -----------------------------------------------------------------------------
 # Core Settings
@@ -33,16 +33,16 @@ ALLOWED_HOSTS = []
 # https://docs.djangoproject.com/en/dev/topics/cache/
 # http://redis.io/topics/lru-cache
 # http://niwibe.github.io/django-redis/
-CACHE_REDIS_DATABASE = '0'
+CACHE_REDIS_DATABASE = "0"
 
 CACHES = {
-    'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/' + CACHE_REDIS_DATABASE,
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-            'IGNORE_EXCEPTIONS': True
-        }
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/" + CACHE_REDIS_DATABASE,
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "IGNORE_EXCEPTIONS": True,
+        },
     }
 }
 
@@ -50,133 +50,121 @@ CSRF_COOKIE_SECURE = True
 CSRF_COOKIE_HTTPONLY = True
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
-DATABASES = {
-}
+DATABASES = {}
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 TEMPLATE_DEBUG = False
 
 INSTALLED_APPS = (
-    'grappelli',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-
-    'require',
-
-    'rest_framework',
-    'taggit',
-    'compressor',
-
-    'wagtail.wagtailcore',
-    'wagtail.wagtailadmin',
-    'wagtail.wagtaildocs',
-    'wagtail.wagtailsnippets',
-    'wagtail.wagtailusers',
-    'wagtail.wagtailimages',
-    'wagtail.wagtailembeds',
-    'wagtail.wagtailsearch',
-    'wagtail.wagtailredirects',
-    'wagtail.wagtailforms',
-    'wagtail.wagtailsites',
-    'wagtail.contrib.wagtailapi',
-    'wagtail.contrib.wagtailroutablepage',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "taggit",
+    "compressor",
+    "wagtail.wagtailcore",
+    "wagtail.wagtailadmin",
+    "wagtail.wagtaildocs",
+    "wagtail.wagtailsnippets",
+    "wagtail.wagtailusers",
+    "wagtail.wagtailimages",
+    "wagtail.wagtailembeds",
+    "wagtail.wagtailsearch",
+    "wagtail.wagtailredirects",
+    "wagtail.wagtailforms",
+    "wagtail.wagtailsites",
+    "wagtail.contrib.wagtailapi",
+    "wagtail.contrib.wagtailroutablepage",
     "wagtail.contrib.table_block",
 )
 
 INSTALLED_APPS += (
     # your project apps here
-    'ddhldap',
-    'cms',
+    "ddhldap",
+    "cms",
     # 'activecollab_digger',
 )
 
-INTERNAL_IPS = ('127.0.0.1', )
+INTERNAL_IPS = ("127.0.0.1",)
 
 # https://docs.djangoproject.com/en/dev/topics/logging/
-LOGGING_ROOT = os.path.join(BASE_DIR, 'logs')
+LOGGING_ROOT = os.path.join(BASE_DIR, "logs")
 LOGGING_LEVEL = logging.WARN
 
 if not os.path.exists(LOGGING_ROOT):
     os.makedirs(LOGGING_ROOT)
 
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': ('%(levelname)s %(asctime)s %(module)s '
-                       '%(process)d %(thread)d %(message)s')
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": (
+                "%(levelname)s %(asctime)s %(module)s "
+                "%(process)d %(thread)d %(message)s"
+            )
         },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        }
+        "simple": {"format": "%(levelname)s %(message)s"},
     },
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.handlers.TimedRotatingFileHandler',
-            'filename': os.path.join(LOGGING_ROOT, 'django.log'),
-            'formatter': 'verbose'
+    "handlers": {
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.handlers.TimedRotatingFileHandler",
+            "filename": os.path.join(LOGGING_ROOT, "django.log"),
+            "formatter": "verbose",
         },
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'simple'
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "simple",
         },
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
+        "mail_admins": {
+            "level": "ERROR",
+            "class": "django.utils.log.AdminEmailHandler",
+        },
     },
-    'loggers': {
-        'django': {
-            'handlers': ['file', 'mail_admins'],
-            'level': LOGGING_LEVEL,
-            'propagate': True
+    "loggers": {
+        "django": {
+            "handlers": ["file", "mail_admins"],
+            "level": LOGGING_LEVEL,
+            "propagate": True,
         },
-        'django_auth_ldap': {
-            'handlers': ['file'],
-            'level': LOGGING_LEVEL,
-            'propagate': True
+        "django_auth_ldap": {
+            "handlers": ["file"],
+            "level": LOGGING_LEVEL,
+            "propagate": True,
         },
-        'mmc': {
-            'handlers': ['file'],
-            'level': LOGGING_LEVEL,
-            'propagate': True
+        "mmc": {"handlers": ["file"], "level": LOGGING_LEVEL, "propagate": True},
+        "elasticsearch": {
+            "handlers": ["file"],
+            "level": LOGGING_LEVEL,
+            "propagate": True,
         },
-        'elasticsearch': {
-            'handlers': ['file'],
-            'level': LOGGING_LEVEL,
-            'propagate': True
-        },
-    }
+    },
 }
 
 
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-
-    'wagtail.wagtailcore.middleware.SiteMiddleware',
-    'wagtail.wagtailredirects.middleware.RedirectMiddleware',
+MIDDLEWARE = (
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.auth.middleware.SessionAuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "wagtail.wagtailcore.middleware.SiteMiddleware",
+    "wagtail.wagtailredirects.middleware.RedirectMiddleware",
 )
 
-ROOT_URLCONF = PROJECT_NAME + '.urls'
+ROOT_URLCONF = PROJECT_NAME + ".urls"
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = ""
 
 WAGTAIL_SITE_NAME = PROJECT_TITLE
 
@@ -185,32 +173,32 @@ ITEMS_PER_PAGE = 10
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.contrib.auth.context_processors.auth',
-                'django.template.context_processors.debug',
-                'django.template.context_processors.media',
-                'django.template.context_processors.request',
-                'django.template.context_processors.static',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.contrib.auth.context_processors.auth",
+                "django.template.context_processors.debug",
+                "django.template.context_processors.media",
+                "django.template.context_processors.request",
+                "django.template.context_processors.static",
+                "django.contrib.messages.context_processors.messages",
                 # 'activecollab_digger.context_processors.activecollab_digger',
             ],
-            'debug': False,
+            "debug": False,
         },
     },
 ]
 
 # https://docs.djangoproject.com/en/dev/topics/i18n/
-LANGUAGE_CODE = 'en-gb'
-TIME_ZONE = 'Europe/London'
+LANGUAGE_CODE = "en-gb"
+TIME_ZONE = "Europe/London"
 USE_I18N = True
 USE_L10N = False
 USE_TZ = True
 
-WSGI_APPLICATION = PROJECT_NAME + '.wsgi.application'
+WSGI_APPLICATION = PROJECT_NAME + ".wsgi.application"
 
 # -----------------------------------------------------------------------------
 # Authentication
@@ -218,7 +206,7 @@ WSGI_APPLICATION = PROJECT_NAME + '.wsgi.application'
 # https://scm.cch.kcl.ac.uk/hg/ddhldap-django
 # -----------------------------------------------------------------------------
 
-AUTH_LDAP_REQUIRE_GROUP = 'cn=mmc,' + LDAP_BASE_OU
+AUTH_LDAP_REQUIRE_GROUP = "cn=mmc," + LDAP_BASE_OU
 
 AUTH_LDAP_ALWAYS_UPDATE_USER = False
 
@@ -231,24 +219,22 @@ AUTH_LDAP_CACHE_GROUPS = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-files
 # -----------------------------------------------------------------------------
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL.strip('/'))
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL.strip("/"))
 
 if not os.path.exists(STATIC_ROOT):
     os.makedirs(STATIC_ROOT)
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'assets'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "assets"),)
 
 STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+    "compressor.finders.CompressorFinder",
 )
 
-STATICFILES_STORAGE = 'require.storage.OptimizedStaticFilesStorage'
-
-MEDIA_URL = STATIC_URL + 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_URL.strip('/'))
+MEDIA_URL = STATIC_URL + "media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_URL.strip("/"))
 
 if not os.path.exists(MEDIA_ROOT):
     os.makedirs(MEDIA_ROOT)
@@ -257,15 +243,15 @@ if not os.path.exists(MEDIA_ROOT):
 # EMAIL SETTINGS
 # -----------------------------------------------------------------------------
 
-EMAIL_HOST = 'smtp.kcl.ac.uk'
+EMAIL_HOST = "smtp.kcl.ac.uk"
 EMAIL_PORT = 25
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
 EMAIL_USE_TLS = False
-DEFAULT_FROM_EMAIL = 'noreply@kcl.ac.uk'
+DEFAULT_FROM_EMAIL = "noreply@kcl.ac.uk"
 # Sender of error messages to ADMINS and MANAGERS
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
-EMAIL_SUBJECT_PREFIX = '[Django {}] '.format(PROJECT_NAME)
+EMAIL_SUBJECT_PREFIX = "[Django {}] ".format(PROJECT_NAME)
 
 # -----------------------------------------------------------------------------
 # Sessions
@@ -287,61 +273,11 @@ COMPRESS_ENABLED = True
 
 COMPRESS_CSS_FILTERS = [
     # CSS minimizer
-    'compressor.filters.cssmin.CSSMinFilter'
+    "compressor.filters.cssmin.CSSMinFilter"
 ]
 
-COMPRESS_PRECOMPILERS = (
-    ('text/x-scss', 'django_libsass.SassCompiler'),
-)
+COMPRESS_PRECOMPILERS = (("text/x-scss", "django_libsass.SassCompiler"),)
 
-# -----------------------------------------------------------------------------
-# Django Grappelli
-# http://django-grappelli.readthedocs.org/en/latest/
-# -----------------------------------------------------------------------------
-
-GRAPPELLI_ADMIN_TITLE = PROJECT_TITLE
-
-# -----------------------------------------------------------------------------
-# Django-Require
-# https://github.com/etianen/django-require
-# -----------------------------------------------------------------------------
-
-# The baseUrl to pass to the r.js optimizer, relative to STATIC_ROOT.
-REQUIRE_BASE_URL = 'js'
-
-# The name of a build profile to use for your project, relative to
-# REQUIRE_BASE_URL. A sensible value would be 'app.build.js'.
-# Leave blank to use the built-in default build profile. Set to False to
-# disable running the default profile (e.g. if only using it to build
-# Standalone Modules)
-REQUIRE_BUILD_PROFILE = False
-
-# The name of the require.js script used by your project, relative to
-# REQUIRE_BASE_URL.
-REQUIRE_JS = '../vendor/requirejs/require.js'
-
-# A dictionary of standalone modules to build with almond.js.
-# See the section on Standalone Modules, below.
-REQUIRE_STANDALONE_MODULES = {
-    'config': {
-        # Where to output the built module, relative to REQUIRE_BASE_URL.
-        'out': 'config-built.js',
-
-        # Optional: A build profile used to build this standalone module.
-        'build_profile': 'config.build.js',
-    }
-}
-
-# Whether to run django-require in debug mode.
-REQUIRE_DEBUG = DEBUG
-
-# A tuple of files to exclude from the compilation result of r.js.
-REQUIRE_EXCLUDE = ('build.txt', )
-
-# The execution environment in which to run r.js: auto, node or rhino.
-# auto will autodetect the environment and make use of node if available and
-# rhino if not.
-REQUIRE_ENVIRONMENT = 'node'
 
 # -----------------------------------------------------------------------------
 # FABRIC
@@ -354,18 +290,18 @@ FABRIC_USER = getpass.getuser()
 # -----------------------------------------------------------------------------
 
 # Google Analytics ID
-GA_ID = 'UA-67707155-5'
+GA_ID = "UA-67707155-5"
 
-WAGTAILSEARCH_RESULTS_TEMPLATE = 'cms/search_results.html'
+WAGTAILSEARCH_RESULTS_TEMPLATE = "cms/search_results.html"
 
 # ActiveCollab API URL
-AC_BASE_URL = 'https://app.activecollab.com/148987'
-AC_API_URL = AC_BASE_URL + '/api/v1/'
+AC_BASE_URL = "https://app.activecollab.com/148987"
+AC_API_URL = AC_BASE_URL + "/api/v1/"
 # ActiveCollab API token
-AC_TOKEN = ''
+AC_TOKEN = ""
 # ActiveCollab project ID
 AC_PROJECT_ID = 769
 # ActiveCollab user ID to create the issues
 AC_USER = 36
 
-LOGIN_URL = '/wagtail/login/'
+LOGIN_URL = "/wagtail/login/"
